37f1e337f6a4022af28738942ab57b521ff0e33b
JoaoLemos00 <joao.lemos2703@gmail.com>
2023-09-15 16:21:44 -0300

    v1

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from personal.models import Question
 # Create your views here.
 
 def home_screen_view(request):
-    return render(request, "base.html", {})
+
+    context = {}
+    questions = Question.objects.all()
+    context['questions'] = questions
+
+    return render(request, "personal/home.html", context)
